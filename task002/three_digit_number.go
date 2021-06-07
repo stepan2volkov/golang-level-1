7243880b5f7784cc7d0a95bdc1c8710098b8e488
Stepan Volkov <stepan2volkov@gmail.com>
2021-06-07 12:16:00 +0500

    Task 2. Basic Types. Input-Output

---
+++ task002/three_digit_number.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number int16
+	fmt.Print("Enter three digit number: ")
+	fmt.Scanln(&number)
+
+	if number < 100 || number > 999 {
+		fmt.Println("The number should contain three digit.")
+		return
+	}
+
+	units := number % 10
+	number = (number - units) / 10
+	tens := number % 10
+	hundreds := (number - tens) / 10
+
+	fmt.Printf("Hundreds: %d, tens: %d, units: %d\n", hundreds, tens, units)
+}
