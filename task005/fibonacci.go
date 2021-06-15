83ca72bd2d5d092f5a912847517fa7aa1d670eb6
Stepan Volkov <stepan2volkov@gmail.com>
2021-06-16 00:47:35 +0500

    Algorithm Fibobacci implemented

---
+++ task005/fibonacci.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+var cacheCalls uint = 0
+var calcCalls uint = 0
+
+// Удобно для сбора статистики создать алиас и методы для него
+type FibonacciCache map[uint]uint
+
+func (cache FibonacciCache) put(n, number uint) {
+	cache[n] = number
+}
+
+func (cache FibonacciCache) get(n uint) (uint, bool) {
+	value, ok := cache[n]
+	if ok {
+		// Фиксируем только те вызовы, которые были полезны
+		cacheCalls++
+	}
+	return value, ok
+}
+
+var fibonacciNumbers = FibonacciCache{}
+
+func fibonacciRecursive(n uint) (number uint) {
+	number, ok := fibonacciNumbers.get(n)
+	if ok {
+		return
+	}
+	calcCalls++
+	defer func() {
+		fibonacciNumbers.put(n, number)
+	}()
+	if n < 2 {
+		number = n
+		return
+	}
+	number = fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
+	return
+}
+
+func main() {
+	var fibonacciNum uint
+
+	fmt.Print("Enter Fibonacci Num: ")
+	fmt.Scanln(&fibonacciNum)
+
+	fmt.Println("Result:", fibonacciRecursive(fibonacciNum))
+
+	// Печать статистики
+	fmt.Printf("Cache: %d, Calculation: %d\n", cacheCalls, calcCalls)
+}
