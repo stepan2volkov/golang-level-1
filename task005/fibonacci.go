fa734c44c89cedbc6b75934421e7a78b2515d895
Stepan Volkov <stepan2volkov@gmail.com>
2021-06-22 17:13:07 +0500

    Replace uint on big.Int (package "math/big")

--- task005/fibonacci.go
+++ task005/fibonacci.go
@@ -2,37 +2,40 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"os"
 )
 
 type Fibonacci struct {
-	cache map[uint]uint
+	cache map[int64]*big.Int
 }
 
-func (f *Fibonacci) Calculate(n uint) (result uint) {
+func (f *Fibonacci) Calculate(n int64) (result *big.Int) {
 	result, exists := f.cache[n]
 	if exists {
 		return
+	} else {
+		result = new(big.Int)
 	}
 	defer func() {
 		f.cache[n] = result
 	}()
 	if n < 2 {
-		result = n
+		result = big.NewInt(n)
 	} else {
-		result = f.Calculate(n-1) + f.Calculate(n-2)
+		result.Add(f.Calculate(n-1), f.Calculate(n-2))
 	}
 	return
 }
 
 func New() *Fibonacci {
-	cache := make(map[uint]uint)
+	cache := make(map[int64]*big.Int)
 	return &Fibonacci{cache}
 }
 
 func main() {
 	f := New()
-	var fibonacciNum uint
+	var fibonacciNum int64
 
 	fmt.Print("Enter Fibonacci Num: ")
 	if _, err := fmt.Scanln(&fibonacciNum); err != nil {
