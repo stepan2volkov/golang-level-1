34247354ef41e78f27bf88c5b478f600ef1e135c
Stepan Volkov <stepan2volkov@gmail.com>
2021-06-22 17:50:18 +0500

    Ability to disable cache introduced. Benchmark realized.

--- task005/fibonacci_test.go
+++ task005/fibonacci_test.go
@@ -3,14 +3,14 @@ package main
 import "testing"
 
 func BenchmarkFibonacciWithouCache10(b *testing.B) {
-	// run the Fib function b.N times
+	// calculate fibonacci numbers without cache b.N times
 	for n := 0; n < b.N; n++ {
 		f := New(false)
 		f.Calculate(10)
 	}
 }
 func BenchmarkFibonacciCache10(b *testing.B) {
-	// run the Fib function b.N times
+	// calculate fibonacci numbers with cache b.N times
 	for n := 0; n < b.N; n++ {
 		f := New(true)
 		f.Calculate(10)
